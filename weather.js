a06fd2779bc3cc562adfad93dad1e3e55f3c2f1b
Jamall Malik <mimsjamall79@gmail.com>
2022-06-23 23:22:13 -0400

    completed

--- weather.js
+++ weather.js
@@ -2,16 +2,15 @@
 
 const axios = require('axios');
 
-async function getWeather(request, response) {
+async function getWeather(cityName) {
 
-  let searchQueryCity = request.query.searchQueryCity;
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQueryCity}&key=${process.env.WEATHER_API_KEY}&days=3&lat=23&lon=155`;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${process.env.WEATHER_API_KEY}&days=3&lat=23&lon=155`;
   let cityWeather = await axios.get(url);
   let selectedCity = cityWeather.data.data.map(dailyWeather => {
     return new Forecast(dailyWeather);
   });
 
-  response.send(selectedCity);
+  return selectedCity;
 
 }
 
